84cf77ffb129547bc28709e2f4851f515514a302
Mariam Mounier <mariam.mostafa1989@gmail.com>
2022-11-02 07:59:03 +0200

    Add files via upload

--- functions.py
+++ functions.py
@@ -1,12 +1,116 @@
+import random 
+import streamlit as st
 import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import librosa
+import librosa.display
+import streamlit_vertical_slider  as svs
+from scipy.io import wavfile as wav
+from scipy.fftpack import fft
+from scipy.fft import fft, fftfreq, fftshift
 
-class variabls:
-    
-    current_slider_values=np.zeros(50)
-    labels_values=np.zeros(50)
-    ranges_values=np.zeros(50)
-    uploaded_audio_time=[]
-    uploaded_audio_Yamp=[]
 
-def set_labels(mode_selector):
-    print('set_labels')
+
+#  ----------------------------------- FOURIER TRANSFORM FUNCTION ---------------------------------------------------
+def fourier_transform(df):
+    # Getting df x_axis and y_axis
+    list_of_columns = df.columns
+    df_x_axis = (df[list_of_columns[0]])
+    df_y_axis = (df[list_of_columns[1]])
+
+    # Frequency domain representation
+    fourier_transform = np.fft.fft(df_y_axis)
+
+    # Do an inverse Fourier transform on the signal
+    inverse_fourier = np.fft.ifft(fourier_transform)
+
+    # Create subplot
+    figure, axis = plt.subplots()
+    plt.subplots_adjust(hspace=1)
+
+    # Frequency domain representation
+    axis.set_title('Fourier transform depicting the frequency components')
+    axis.plot(df_x_axis, abs(fourier_transform))
+    axis.set_xlabel('Frequency')
+    axis.set_ylabel('Amplitude')
+
+    st.plotly_chart(figure,use_container_width=True)
+
+    return inverse_fourier, fourier_transform
+
+#  ----------------------------------- INVERSE FOURIER TRANSFORM FUNCTION ---------------------------------------------------
+def fourier_inverse_transform(inverse_fourier,df):
+    # Getting df x_axis and y_axis
+    list_of_columns = df.columns
+    df_x_axis = list(df[list_of_columns[0]])
+    df_y_axis = list(df[list_of_columns[1]])
+
+    # Create subplot
+    figure, axis = plt.subplots()
+    plt.subplots_adjust(hspace=1)
+
+    # Frequency domain representation
+    axis.set_title('Inverse Fourier transform depicting the frequency components')
+    axis.plot(df_x_axis, inverse_fourier)
+    axis.set_xlabel('Frequency')
+    axis.set_ylabel('Amplitude')
+
+    fig,ax = plt.subplots()
+    ax.set_title('The Actual Data')
+    ax.plot(df_x_axis,df_y_axis)
+    ax.set_xlabel('Frequency')
+    ax.set_ylabel('Amplitude')
+
+    st.plotly_chart(figure,use_container_width=True)
+    st.plotly_chart(fig,use_container_width=True)
+
+def wave_ranges(fourier_transform):
+    st.write(abs(fourier_transform))
+
+#  ----------------------------------- CREATING SLIDERS ---------------------------------------------------------------
+def creating_sliders(names_list):
+
+    # names_list = [('Megzawy', 100),('Magdy', 150)]
+    columns = st.columns(len(names_list))
+    boundary = int(50)
+    sliders_values = []
+    sliders = {}
+
+    for index, tuple in enumerate(names_list): # ---> [ { 0, ('Megzawy', 100) } , { 1 , ('Magdy', 150) } ]
+        # st.write(index)
+        # st.write(i)
+        min_value = tuple[1] - boundary
+        max_value = tuple[1] + boundary
+        key = f'member{random.randint(0,10000000000)}'
+        with columns[index]:
+            sliders[f'slidergroup{key}'] = svs.vertical_slider(key=key, default_value=tuple[1], step=1, min_value=min_value, max_value=max_value)
+            if sliders[f'slidergroup{key}'] == None:
+                sliders[f'slidergroup{key}'] = tuple[1]
+            sliders_values.append((tuple[0], sliders[f'slidergroup{key}']))
+            # st.write(sliders_values)
+
+#  ----------------------------------- PLOTTING AUDIO ---------------------------------------------------------------
+# def plotting(amplitude, frequency):
+#     # Plotting audio Signal
+#     fig = plt.figure(figsize=[10,6])
+#     librosa.display.waveshow(amplitude, frequency)
+#     st.pyplot(fig)
+#-------------------------------------- Fourier Transform on Audio ----------------------------------------------------
+def fourier_for_audio(uploaded_file):
+    sample_rate, amplitude = wav.read(uploaded_file) #kam sample fl sec fl track,amplitude l data
+    amplitude = np.frombuffer(amplitude, "int32") #str code khd mn dof3t 4 - 3 ayam search
+    fft_out = fft(amplitude)          #el fourier bt3t el amplitude eli hnshtghl beha fl equalizer
+    fft_out=np.abs(fft_out)[:len(amplitude)//2] #np.abs 3shan el rsm
+    # plt.plot(amplitude, np.abs(fft_out))
+    # plt.show() satren code mbyrsmosh haga 
+    x_axis_fourier = fftfreq(len(amplitude),(1/sample_rate))[:len(amplitude)//2] #3shan mbd2sh mn -ve
+    return x_axis_fourier,fft_out
+
+def plotting(x_axis_fourier,fft_out):
+ # Plotting audio Signal
+    figure, axis = plt.subplots()
+    plt.subplots_adjust(hspace=1)
+    axis.plot(x_axis_fourier,fft_out)
+    st.plotly_chart(figure,use_container_width=True) 
+#-------------tagroba fashla------------
--- main.py
+++ main.py
@@ -1,106 +1,58 @@
 import streamlit as st
-from email.mime import audio
-import numpy as np  
 import pandas as pd
-import plotly.graph_objects as go
-import plotly.express as px
-import librosa as lr
-from streamlit_option_menu import option_menu
-import functions
+import functions as fn
+import librosa
+from scipy.io import wavfile as wav
+
+st.set_page_config(layout="wide")
+
+# ------------------------------------------------------------------------------------ uploaded_file Browsing Button
+uploaded_file = st.file_uploader(label="Uploading Signal", type = ['csv',"wav"])
+
+# ------------------------------------------------------------------------------------Calling Main Functions
+if uploaded_file is not None:
+
+    # USER OPTIONS
+    radio_button = st.radio("",["Default Signal", "Music", "Vowels", "Arrhythima", "Optional"])
+
+    if radio_button == "Default Signal":
+        names_list = [('A', 100),('B', 150),('C', 75),('D', 25),('E', 150),('F', 60),('G', 86),('H', 150),('E', 150),('G', 25),('K', 99),('L', 150),
+                        ('M', 150),('M', 55),('N', 150)]
+        fn.creating_sliders(names_list)
+
+    elif radio_button == "Music":
+        names_list = [('Megzawy', 100),('Magdy', 150)]
+        fn.creating_sliders(names_list)
+
+    elif radio_button == "Vowels":
+        names_list = [('Amr', 100),('Sameh', 150),]
+        fn.creating_sliders(names_list)
+
+    elif radio_button == "Arrhythima":
+        names_list = [('Mariam', 100),('Taha', 150),]
+        fn.creating_sliders(names_list)
+    else:
+        names_list = [('Ahmed', 100),('Youssef', 150),]
+        fn.creating_sliders(names_list)
+
+    file_name = uploaded_file.type
+    file_extension = file_name[-3:]
 
 
-st.set_page_config(page_title="Equalizer studio",
-                   page_icon="📶", layout="wide",
-                   initial_sidebar_state="expanded",
-                   )
+    if file_extension == "csv":
+        df = pd.read_csv(uploaded_file)
+        inverseFourier, fourierTransform = fn.fourier_transform(df)
+        fn.fourier_inverse_transform(inverseFourier,df)
+        fn.wave_ranges(fourierTransform)
+    else:
+        # st.audio(uploaded_file, format='audio/ogg')         # displaying the audio player
+        # amplitude, frequency = librosa.load(uploaded_file)  # getting audio attributes which are amplitude and frequency (number of frames per second)
+        # fn.plotting(amplitude, frequency) #librosa feha moshkla f fourier bokra inshalah afhmhalko
+        st.audio(uploaded_file, format='audio/wav') 
+        xf,fft_out = fn.fourier_for_audio(uploaded_file)
+        fn.plotting(xf,fft_out)
 
-# # styles from css file
-# with open('style.css') as css:
-#     st.markdown(f'<style>{css.read()}</style>', unsafe_allow_html=True)
-
-# Styling of the sliders
-ColorMinMax = st.markdown(''' <style> div.stSlider > div[data-baseweb = "slider"] > div[data-testid="stTickBar"] > div {
-    background: rgb(1 1 1 / 0%); } </style>''', unsafe_allow_html=True)
-
-
-Slider_Cursor = st.markdown(''' <style> div.stSlider > div[data-baseweb="slider"] > div > div > div[role="slider"]{
-    background-color: rgb(14, 38, 74); box-shadow: rgb(14 38 74 / 20%) 0px 0px 0px 0.2rem;} </style>''', unsafe_allow_html=True)
-
-
-Slider_Number = st.markdown(''' <style> div.stSlider > div[data-baseweb="slider"] > div > div > div > div
-                                { color: rgb(14, 38, 74); 
-                                  font-weight: bold
-                                } </style>''', unsafe_allow_html=True)
-
-# Styling of the buttons
-m = st.markdown("""
-<style>
-div.stButton > button:first-child {
-    background-color: #191970;
-    color:#ffffff;
-    border-radius: 60px;
-    font-weight: bold
-}
-div.stButton > button:hover {
-    background-color:#B0C4DE;
-    color:#191970;
-    }
-[data-testid="stSidebar"][aria-expanded="true"] > div:first-child {
-        width: 300px;
-        heigth: 100;
-    }
-    [data-testid="stSidebar"][aria-expanded="false"] > div:first-child {
-        width: 500px;
-        margin-left: -500px;
-    }
-</style>""", unsafe_allow_html=True)
-# ***************************************************************************#
-freq_1 = st.sidebar.slider('Choose the first frequency', 0, 100, 0)
-st.session_state["frequency_1"] = freq_1
-freq_2 = st.sidebar.slider('Choose the second frequency', 0, 100, 0)
-st.session_state["frequency_2"] = freq_2
-freq_3 = st.sidebar.slider('Choose the third frequency', 0, 100, 0)
-st.session_state["frequency_3"] = freq_3
-freq_4 = st.sidebar.slider('Choose the fourth frequency', 0, 100, 0)
-st.session_state["frequency_4"] = freq_4
-freq_5 = st.sidebar.slider('Choose the fifth frequency', 0, 100, 0)
-st.session_state["frequency_5"] = freq_5
-freq_6 = st.sidebar.slider('Choose the sixth frequency', 0, 100, 0)
-st.session_state["frequency_6"] = freq_6
-freq_7 = st.sidebar.slider('Choose the seventh frequency', 0, 100, 0)
-st.session_state["frequency_7"] = freq_7
-freq_8 = st.sidebar.slider('Choose the eigth frequency', 0, 100, 0)
-st.session_state["frequency_8"] = freq_8
-freq_9 = st.sidebar.slider('Choose the ningth frequency', 0, 100, 0)
-st.session_state["frequency_9"] = freq_9
-freq_10 = st.sidebar.slider('Choose the tenth frequency', 0, 100, 0)
-st.session_state["frequency_10"] = freq_10
-
-Signal_type = st.selectbox(
-    'what is the required type of the signal?',
-    ('audio','Alphapetic', 'musical', 'Ecg' ,'else'))
+else:
+    pass
 
 
-Uploaded_fig= px.density_heatmap(data_frame=[{}])
-
-uploaded_file =st.file_uploader('upload the signal file',['csv','ogg','wav'] , help='upload your signal file' )
-if (uploaded_file):
-
-    st.audio(uploaded_file, format='audio/wav',start_time=10)
-    # analyising uploaded audio --> getting x ,y
-    audio_amp,sampFreq= lr.load(uploaded_file)
-    time_axis =np.arange(0,len(audio_amp))/sampFreq
-    Uploaded_fig.add_trace(go.Scatter(x=time_axis[:1000], y=audio_amp[:1000],))
-    # updating data in functions
-    functions.variabls.uploaded_audio_time=time_axis
-    functions.variabls.uploaded_audio_time=audio_amp   
-
-
-# FRONT uii
-
-st.title("Equalizer:")
-fig,fig2 = st.columns((2))
-
-with fig:
-    st.write('audio signal')
-    st.write(Uploaded_fig)
