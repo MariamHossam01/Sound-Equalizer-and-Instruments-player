431316b77bc099597967990db1272653aade108d
Mariam Mounier <mariam.mostafa1989@gmail.com>
2022-11-05 01:19:53 +0200

    audio_function
    Needs to update layout
    Save your files somewhere else to avoid conflit

--- functions.py
+++ functions.py
@@ -3,114 +3,77 @@ import streamlit as st
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-import librosa
-import librosa.display
 import streamlit_vertical_slider  as svs
-from scipy.io import wavfile as wav
 from scipy.fftpack import fft
 from scipy.fft import fft, fftfreq, fftshift
+from scipy.fft import rfft, rfftfreq
+from scipy.fft import irfft
+from scipy.io.wavfile import write
+from scipy.signal import find_peaks
+import wave
+import IPython.display as ipd
 
-
-
-#  ----------------------------------- FOURIER TRANSFORM FUNCTION ---------------------------------------------------
-def fourier_transform(df):
-    # Getting df x_axis and y_axis
-    list_of_columns = df.columns
-    df_x_axis = (df[list_of_columns[0]])
-    df_y_axis = (df[list_of_columns[1]])
-
-    # Frequency domain representation
-    fourier_transform = np.fft.fft(df_y_axis)
-
-    # Do an inverse Fourier transform on the signal
-    inverse_fourier = np.fft.ifft(fourier_transform)
-
-    # Create subplot
-    figure, axis = plt.subplots()
-    plt.subplots_adjust(hspace=1)
-
-    # Frequency domain representation
-    axis.set_title('Fourier transform depicting the frequency components')
-    axis.plot(df_x_axis, abs(fourier_transform))
-    axis.set_xlabel('Frequency')
-    axis.set_ylabel('Amplitude')
-
-    st.plotly_chart(figure,use_container_width=True)
-
-    return inverse_fourier, fourier_transform
-
-#  ----------------------------------- INVERSE FOURIER TRANSFORM FUNCTION ---------------------------------------------------
-def fourier_inverse_transform(inverse_fourier,df):
-    # Getting df x_axis and y_axis
-    list_of_columns = df.columns
-    df_x_axis = list(df[list_of_columns[0]])
-    df_y_axis = list(df[list_of_columns[1]])
-
-    # Create subplot
-    figure, axis = plt.subplots()
-    plt.subplots_adjust(hspace=1)
-
-    # Frequency domain representation
-    axis.set_title('Inverse Fourier transform depicting the frequency components')
-    axis.plot(df_x_axis, inverse_fourier)
-    axis.set_xlabel('Frequency')
-    axis.set_ylabel('Amplitude')
-
-    fig,ax = plt.subplots()
-    ax.set_title('The Actual Data')
-    ax.plot(df_x_axis,df_y_axis)
-    ax.set_xlabel('Frequency')
-    ax.set_ylabel('Amplitude')
-
-    st.plotly_chart(figure,use_container_width=True)
-    st.plotly_chart(fig,use_container_width=True)
-
-def wave_ranges(fourier_transform):
-    st.write(abs(fourier_transform))
-
-#  ----------------------------------- CREATING SLIDERS ---------------------------------------------------------------
-def creating_sliders(names_list):
-
-    # names_list = [('Megzawy', 100),('Magdy', 150)]
-    columns = st.columns(len(names_list))
-    boundary = int(50)
-    sliders_values = []
-    sliders = {}
-
-    for index, tuple in enumerate(names_list): # ---> [ { 0, ('Megzawy', 100) } , { 1 , ('Magdy', 150) } ]
-        # st.write(index)
-        # st.write(i)
-        min_value = tuple[1] - boundary
-        max_value = tuple[1] + boundary
-        key = f'member{random.randint(0,10000000000)}'
-        with columns[index]:
-            sliders[f'slidergroup{key}'] = svs.vertical_slider(key=key, default_value=tuple[1], step=1, min_value=min_value, max_value=max_value)
-            if sliders[f'slidergroup{key}'] == None:
-                sliders[f'slidergroup{key}'] = tuple[1]
-            sliders_values.append((tuple[0], sliders[f'slidergroup{key}']))
-            # st.write(sliders_values)
-
-#  ----------------------------------- PLOTTING AUDIO ---------------------------------------------------------------
-# def plotting(amplitude, frequency):
-#     # Plotting audio Signal
-#     fig = plt.figure(figsize=[10,6])
-#     librosa.display.waveshow(amplitude, frequency)
-#     st.pyplot(fig)
+#  ----------------------------------- TRIAL FOURIER TRANSFORM FUNCTION ---------------------------------------------------
 #-------------------------------------- Fourier Transform on Audio ----------------------------------------------------
-def fourier_for_audio(uploaded_file):
-    sample_rate, amplitude = wav.read(uploaded_file) #kam sample fl sec fl track,amplitude l data
-    amplitude = np.frombuffer(amplitude, "int32") #str code khd mn dof3t 4 - 3 ayam search
-    fft_out = fft(amplitude)          #el fourier bt3t el amplitude eli hnshtghl beha fl equalizer
-    fft_out=np.abs(fft_out)[:len(amplitude)//2] #np.abs 3shan el rsm
-    # plt.plot(amplitude, np.abs(fft_out))
-    # plt.show() satren code mbyrsmosh haga 
-    x_axis_fourier = fftfreq(len(amplitude),(1/sample_rate))[:len(amplitude)//2] #3shan mbd2sh mn -ve
-    return x_axis_fourier,fft_out
+def uniform_range_mode(audio_file):
+    st.audio(audio_file, format='audio/wav') # displaying the audio
+    obj = wave.open(audio_file, 'rb')
+    sample_rate = obj.getframerate()      # number of samples per second
+    n_samples   = obj.getnframes()        # total number of samples in the whole audio
+    duration    = n_samples / sample_rate # duration of the audio file
+    signal_wave = obj.readframes(-1)      # amplitude of the sound
 
-def plotting(x_axis_fourier,fft_out):
- # Plotting audio Signal
-    figure, axis = plt.subplots()
-    plt.subplots_adjust(hspace=1)
-    axis.plot(x_axis_fourier,fft_out)
-    st.plotly_chart(figure,use_container_width=True) 
-#-------------tagroba fashla------------
+    signal_y_axis = np.frombuffer(signal_wave, dtype=np.int16)
+    signal_x_axis = np.linspace(0, duration, len(signal_y_axis))
+
+    yf = rfft(signal_y_axis) # returns complex numbers of the y axis in the data frame
+    xf = rfftfreq(len(signal_y_axis), (signal_x_axis[1]-signal_x_axis[0])) # returns the frequency x axis after fourier transform
+
+    st.write(sample_rate)
+    points_per_freq = len(xf) / (xf[-1]) # NOT UNDERSTANDABLE 
+    
+    fig, axs = plt.subplots()
+    fig.set_size_inches(14,5)
+    plt.plot(signal_x_axis, signal_y_axis) #plotting fourier
+    st.plotly_chart(fig)
+
+    slider_range_1 = st.slider(label='Slider_1' , min_value=0, max_value=10, value=1, step=1, key="1")
+    slider_range_2 = st.slider(label='Slider_2' , min_value=0, max_value=10, value=1, step=1, key="2")
+    slider_range_3 = st.slider(label='Slider_3' , min_value=0, max_value=10, value=1, step=1, key="3")
+    slider_range_4 = st.slider(label='Slider_4' , min_value=0, max_value=10, value=1, step=1, key="4")
+    slider_range_5 = st.slider(label='Slider_5' , min_value=0, max_value=10, value=1, step=1, key="5")
+    slider_range_6 = st.slider(label='Slider_6' , min_value=0, max_value=10, value=1, step=1, key="6")
+    slider_range_7 = st.slider(label='Slider_7' , min_value=0, max_value=10, value=1, step=1, key="7")
+    slider_range_8 = st.slider(label='Slider_8' , min_value=0, max_value=10, value=1, step=1, key="8")
+    slider_range_9 = st.slider(label='Slider_9' , min_value=0, max_value=10, value=1, step=1, key="9")
+    slider_range_10= st.slider(label='Slider_10', min_value=0, max_value=10, value=1, step=1, key="10")
+
+
+
+
+
+    yf[int(points_per_freq*0)   :int(points_per_freq* 1000)] *= slider_range_1
+    yf[int(points_per_freq*1000):int(points_per_freq* 2000)] *= slider_range_2
+    yf[int(points_per_freq*2000):int(points_per_freq*3000)]  *= slider_range_3
+    yf[int(points_per_freq*3000):int(points_per_freq*4000)]  *= slider_range_4
+    yf[int(points_per_freq*4000):int(points_per_freq*5000)]  *= slider_range_5
+    yf[int(points_per_freq*5000):int(points_per_freq*6000)]  *= slider_range_6
+    yf[int(points_per_freq*6000):int(points_per_freq*7000)]  *= slider_range_7
+    yf[int(points_per_freq*7000):int(points_per_freq*8000)]  *= slider_range_8
+    yf[int(points_per_freq*8000):int(points_per_freq*9000)]  *= slider_range_9
+    yf[int(points_per_freq*9000):int(points_per_freq*10000)] *= slider_range_10
+    # fig2, axs2 = plt.subplots()
+    # fig2.set_size_inches(14,5)
+    # plt.plot(xf,np.abs(yf)) # ploting signal after modifying
+    # st.plotly_chart(fig2,use_container_width=True)
+
+
+
+
+    modified_signal         = irfft(yf)                 # returns the inverse transform after modifying it with sliders 
+    modified_signal_channel = np.int16(modified_signal) # returns two channels 
+
+
+    write   ("Equalized_Music.wav", sample_rate, modified_signal_channel) # creates the modified song
+    st.audio("Equalized_Music.wav", format='audio/wav')
+
--- main.py
+++ main.py
@@ -1,58 +1,34 @@
 import streamlit as st
 import pandas as pd
 import functions as fn
-import librosa
-from scipy.io import wavfile as wav
 
 st.set_page_config(layout="wide")
 
-# ------------------------------------------------------------------------------------ uploaded_file Browsing Button
-uploaded_file = st.file_uploader(label="Uploading Signal", type = ['csv',"wav"])
+# ------------------------------------------------------------------------------------ Uploaded File Browsing Button
+uploaded_file = st.file_uploader(label="Uploading Signal", type = ['csv',".wav"])
 
-# ------------------------------------------------------------------------------------Calling Main Functions
+# ------------------------------------------------------------------------------------ Calling Main Functions
 if uploaded_file is not None:
-
-    # USER OPTIONS
-    radio_button = st.radio("",["Default Signal", "Music", "Vowels", "Arrhythima", "Optional"])
-
-    if radio_button == "Default Signal":
-        names_list = [('A', 100),('B', 150),('C', 75),('D', 25),('E', 150),('F', 60),('G', 86),('H', 150),('E', 150),('G', 25),('K', 99),('L', 150),
-                        ('M', 150),('M', 55),('N', 150)]
-        fn.creating_sliders(names_list)
-
-    elif radio_button == "Music":
-        names_list = [('Megzawy', 100),('Magdy', 150)]
-        fn.creating_sliders(names_list)
-
-    elif radio_button == "Vowels":
-        names_list = [('Amr', 100),('Sameh', 150),]
-        fn.creating_sliders(names_list)
-
-    elif radio_button == "Arrhythima":
-        names_list = [('Mariam', 100),('Taha', 150),]
-        fn.creating_sliders(names_list)
-    else:
-        names_list = [('Ahmed', 100),('Youssef', 150),]
-        fn.creating_sliders(names_list)
-
+    # Determining whether the file is csv or wav
     file_name = uploaded_file.type
     file_extension = file_name[-3:]
 
+    # USER OPTIONS
+    radio_button = st.radio("",["Uniform Range Mode", "Music", "Vowels", "Arrhythima", "Optional"])
 
-    if file_extension == "csv":
-        df = pd.read_csv(uploaded_file)
-        inverseFourier, fourierTransform = fn.fourier_transform(df)
-        fn.fourier_inverse_transform(inverseFourier,df)
-        fn.wave_ranges(fourierTransform)
+    if radio_button == "Uniform Range Mode":
+        fn.uniform_range_mode(uploaded_file)
+
+    elif radio_button == "Music":
+        pass
+    elif radio_button == "Vowels":
+        pass
+
+    elif radio_button == "Arrhythima":
+        if file_extension == "csv":
+            pass
     else:
-        # st.audio(uploaded_file, format='audio/ogg')         # displaying the audio player
-        # amplitude, frequency = librosa.load(uploaded_file)  # getting audio attributes which are amplitude and frequency (number of frames per second)
-        # fn.plotting(amplitude, frequency) #librosa feha moshkla f fourier bokra inshalah afhmhalko
-        st.audio(uploaded_file, format='audio/wav') 
-        xf,fft_out = fn.fourier_for_audio(uploaded_file)
-        fn.plotting(xf,fft_out)
-
+        pass
 else:
     pass
-
 
